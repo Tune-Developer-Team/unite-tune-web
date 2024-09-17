6512acb2edbb23426806a4f43f427f949c545b7b
yamashita <yamashita.antapp@gmail.com>
2024-09-17 15:55:06 +0900

    🔧Seed詳細画面の描画更新のタイミング修正

--- src/scene/seedDetail/seedDetailView.tsx
+++ src/scene/seedDetail/seedDetailView.tsx
@@ -8,11 +8,13 @@ import {useRecoilState} from "recoil";
 import {navigationState} from "../../atoms/NavigationState";
 import {authenticationState} from "../../atoms/AuthenticationState";
 import {profileState} from "../../atoms/ProfileState";
+import {SeedDetail} from "../../models/Seed/SeedDetail/seedDetail";
 
 const seedDetailViewModel = new SeedDetailViewModel();
 
 const SeedDetailView = () => {
     const [viewModel, setViewModel] = useState<SeedDetailViewModel>(seedDetailViewModel);
+    const [SeedDetail, setSeedDetail] = useState<SeedDetail>(viewModel.seedDetail);
     const [navigation, setNavigation] = useRecoilState(navigationState);
     const [authState, setAuthentication] = useRecoilState(authenticationState);
     const [profile, setProfile] = useRecoilState(profileState);
@@ -21,13 +23,14 @@ const SeedDetailView = () => {
     const seedId = params.seedId as string;
 
     const setUp = async (): Promise<void> => {
-        await viewModel.setUp({
+        const newViewModel = await viewModel.setUp({
             authentication: {
                 accessToken: authState.accessToken,
                 uid: authState.uid,
                 email: authState.email
             }, seedId: seedId,
         });
+        setSeedDetail(viewModel.seedDetail);
     }
 
     useEffect(() => {
@@ -46,31 +49,34 @@ const SeedDetailView = () => {
             <Grid container spacing={2} className={"projectByLanguage"}>
                 <Grid sx={{textAlign: "start"}} xs={12} sm={12} md={12} lg={12}>
                     <Typography variant="h4" component="div" sx={{textAlign: "center"}}>
-                        Seed 詳細
+                        {viewModel.seedDetail.title}
                     </Typography>
                 </Grid>
                 <Grid xs={6} sm={6} md={6} lg={6}>
-                    <>
-                        SeedId {params.seedId}
-                    </>
                     <Box sx={{textAlign:"start"}}>
 
                     </Box>
                 </Grid>
+
                 <Grid xs={6} sm={6} md={6} lg={6} sx={{textAlign: "center"}}>
                     <Box sx={{textAlign:"start"}}>
                         <Typography>
-                           詳細
+                            {viewModel.seedDetail.description}
+                        </Typography>
+                        <Typography>
+                            期間は{viewModel.seedDetail.termsFrom}（unix）から{viewModel.seedDetail.termsTo}（unix）まで
+                        </Typography>
+                        <Typography>
+                            関連ワード {viewModel.seedDetail.hashTagStringList}
                         </Typography>
                     </Box>
                 </Grid>
-
 
                 <Grid xs={3} sm={3} md={3} lg={3} >
                 </Grid>
                 <Grid sx={{textAlign: "center"}} xs={6} sm={6} md={6} lg={6}>
                     <Typography variant="h4" component="div" sx={{textAlign: "center"}}>
-                        コメント
+                        {viewModel.seedDetail.benefit}
                     </Typography>
                 </Grid>
                 <Grid xs={3} sm={3} md={3} lg={3} >
